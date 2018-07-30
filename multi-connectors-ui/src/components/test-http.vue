86f107d46ca23eafa24c12e893ac82f14baa413f
liumapp <liumapp.com@gmail.com>
2018-07-30 10:52:39 +0800

    update test-http

--- multi-connectors-ui/src/components/test-http.vue
+++ multi-connectors-ui/src/components/test-http.vue
@@ -7,7 +7,26 @@
  */
 <template>
   <div>
-    this is http test page .
+    <br>
+    <Row>
+      <Col span="4" offset="8"> 发起GET请求获取响应：</Col>
+      <Col span="8">
+        <Button type="success" @click="submitGet">点我</Button>
+      </Col>
+    </Row>
+    <br>
+    <Row>
+      <Col span="4" offset="8">发起POST请求获取响应：</Col>
+      <Col span="8">
+        <Button type="success" @click="submitPost">再点我呀</Button>
+      </Col>
+    </Row>
+    <br>
+    <Row>
+      <Col span="4" offset="8">
+        <Button type="primary" @click="next">去下一步看看</Button>
+      </Col>
+    </Row>
   </div>
 </template>
 <script>
