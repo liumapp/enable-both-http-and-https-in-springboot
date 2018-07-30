852a8204dfbbc91389495461c8e79a16f8bf918b
liumapp <liumapp.com@gmail.com>
2018-07-30 14:21:28 +0800

    update rest apip g

--- multi-connectors-service/src/main/java/com/liumapp/demo/https/controller/IndexController.java
+++ multi-connectors-service/src/main/java/com/liumapp/demo/https/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.liumapp.demo.https.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,6 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 public class IndexController {
 
+    @RequestMapping("/get")
+    public String autoIncreate (@RequestParam Integer number) {
+        number = number ++;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result", number);
+        return JSON.toJSONString(jsonObject);
+    }
 
+    @RequestMapping("/post")
+    public String autoMulti (@RequestParam Integer number) {
+        number = number * 2;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result", number);
+        return JSON.toJSONString(jsonObject);
+    }
 
 }
